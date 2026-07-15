a1b4294dd78806153dd2b670e9e6649f11150630
neha6868pal-arch <neha6868pal@gmail.com>
2026-07-14 22:07:39 -0600

    Add files via upload

--- JAVA/RomanNumerals.java
+++ JAVA/RomanNumerals.java
@@ -1,23 +1,43 @@
 import javax.swing.*;
-import java.sql.ResultSet;
+import java.lang.reflect.AccessFlag;
 import java.util.Scanner;
 
 public class RomanNumerals {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //要求只能录入数字，将内容变成罗马数字
         //数字转换想数组
-        StringBuilder sb = new StringBuilder();
-//        String[] arr = {"","Ⅰ","Ⅱ","Ⅲ","Ⅳ","Ⅴ","Ⅵ","Ⅶ","Ⅷ","Ⅸ"};
-        char[] arr = {'\0','Ⅰ','Ⅱ','Ⅲ','Ⅳ','Ⅴ','Ⅵ','Ⅶ','Ⅷ','Ⅸ'};
-        System.out.println("请输入数字");
-        sb.append(sc.nextLine());
-        String result = "";
-        for (int i = 0; i < arr.length; i++) {
-             result = sb.toString().replace('i',arr[i]);//因为前面替换的是小写字母i，而非我想要的数字
+        String[] arr = {"","Ⅰ","Ⅱ","Ⅲ","Ⅳ","Ⅴ","Ⅵ","Ⅶ","Ⅷ","Ⅸ"};
+//        char[] arr = {'\0', 'Ⅰ', 'Ⅱ', 'Ⅲ', 'Ⅳ', 'Ⅴ', 'Ⅵ', 'Ⅶ', 'Ⅷ', 'Ⅸ'};
+        while (true){
+            System.out.println("请输入数字");
+            String num = sc.nextLine();
+            boolean flag = checkStr(num);
+            if (flag){
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < num.length(); i++) {
+                    sb.append(arr[num.charAt(i)-48]);
+                }
+                System.out.println(sb);
+                break;
+            }
         }
-        System.out.println(result);
-        //为什么
-        //因为result是局部变量，每次循环都会重新定义，所以只能输出最后一个结果，而不是每次循环的结果
+//        String result = "";
+//        for (int i = 0; i < arr.length; i++) {
+//            result = sb.toString().replace('i',arr[i]);//因为前面替换的是小写字母i，而非我想要的数字
+//        }//这个太逊了，实现不了我想要的效果
+//        System.out.println(result);
+//     }
+    }
+    public static boolean checkStr(String num){
+        boolean flag = true;
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);//现在遍历出字符，就可以根据ascii表来限定数字
+            //怪不得字符有表
+            if (c<'0'||c>'9'){//运算中，直接写字符就行，会自动转为int
+                flag = false;
+            }
+        }
+        return flag;
     }
 }
