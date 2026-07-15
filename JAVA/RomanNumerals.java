f10d67c89e5f6d580b0ab7312dfee4ac0ad0ba48
neha6868pal-arch <neha6868pal@gmail.com>
2026-07-14 21:25:35 -0600

    Add files via upload

---
+++ JAVA/RomanNumerals.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class RomanNumerals {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //要求只能录入数字，将内容变成罗马数字
+        //数字转换想数组
+        StringBuilder sb = new StringBuilder();
+//        String[] arr = {"","Ⅰ","Ⅱ","Ⅲ","Ⅳ","Ⅴ","Ⅵ","Ⅶ","Ⅷ","Ⅸ"};
+        char[] arr = {'\0','Ⅰ','Ⅱ','Ⅲ','Ⅳ','Ⅴ','Ⅵ','Ⅶ','Ⅷ','Ⅸ'};
+        System.out.println("请输入数字");
+        sb.append(sc.nextLine());
+        String result = "";
+        for (int i = 0; i < arr.length; i++) {
+             result = sb.toString().replace('i',arr[i]);
+        }
+        System.out.println(result);
+        //为什么
+        //因为result是局部变量，每次循环都会重新定义，所以只能输出最后一个结果，而不是每次循环的结果
+    }
+}
