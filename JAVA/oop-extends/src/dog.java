982fdad633638dfa0c47c6c6435fb5b1aa976386
汀烟鹭 <neha6868pal@gmail.com>
2026-07-23 14:17:13 +0800

    Add files via upload

--- JAVA/oop-extends/src/dog.java
+++ JAVA/oop-extends/src/dog.java
@@ -1,5 +1,11 @@
 public class dog extends animal{
-    public void lookhome(){
+    public void eat(){
+        System.out.println("吃饭（吃狗粮）");
+    }
+    public void drink(){
+        System.out.println("喝水");
+    }
+    public void lookHome(){
         System.out.println("看家");
     }
 }
