de82c24608ccea131fe4065d05e2bbc0b1097f05
汀烟鹭 <neha6868pal@gmail.com>
2026-07-24 12:24:09 +0800

    Add files via upload

--- JAVA/oop-polymorphism/src/test.java
+++ JAVA/oop-polymorphism/src/test.java
@@ -1,11 +1,11 @@
 public class test {
    public static void main(String[] args){
        Person p = new Person("wang",20);
-       Animal dog = new Dog(2,"黄棕");
+       Dog dog = new Dog(2,"黄棕");
        p.keepPet(dog,"骨头");
 
        Person p2 = new Person("Li",20);
-       Animal cat = new Cat(2,"灰");
-       p2.keepPet(dog,"鱼");
+       Cat cat = new Cat(1,"灰");
+       p2.keepPet(cat,"鱼");
    }
 }
