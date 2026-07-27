da9b3985bd3c7eb9d8bd2098c26837f6bb2d60f3
汀烟鹭 <neha6868pal@gmail.com>
2026-07-27 10:25:53 +0800

    Add files via upload

---
+++ JAVA/puzzleGame/src/com/itheima/ui/itheima/ui/LoginJFrame.java
@@ -0,0 +1,14 @@
+package com.itheima.ui;
+
+import javax.swing.*;
+
+public class LoginJFrame extends JFrame {
+    public LoginJFrame(){
+        this.setSize(480,430);
+        this.setTitle("拼图 登录");
+        this.setAlwaysOnTop(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(3);
+        this.setVisible(true);
+    }
+}
