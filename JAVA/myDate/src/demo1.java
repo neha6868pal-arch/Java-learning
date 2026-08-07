b68a1071c95f198ceab1c7b1b254a2d33706c48d
汀烟鹭 <neha6868pal@gmail.com>
2026-08-07 11:05:47 +0800

    Add files via upload

--- JAVA/myDate/src/demo1.java
+++ JAVA/myDate/src/demo1.java
@@ -3,10 +3,10 @@ import java.util.Date;
 public class demo1 {
     public static void main(String[] args){
       //默认时间后一年
-        Date di = new Date(0L);//L提前转化为long
-        long time = di.getTime();
-        time = time + 1000L*60*60*24*356;
-        di.setTime(time);
+        Date di = new Date(1000L*60*60*24*365);//L提前转化为long
+//        long time = di.getTime();
+//        time = time + 1000L*60*60*24*365;
+//        di.setTime(time);
         System.out.println(di);//di本身是默认时间，传入的是后移时间
     }
 }
