3957532e62d6770536b5d266b4865585e5bee1d2
汀烟鹭 <neha6868pal@gmail.com>
2026-08-07 11:02:58 +0800

    Add files via upload

---
+++ JAVA/myDate/src/demo1.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class demo1 {
+    public static void main(String[] args){
+      //默认时间后一年
+        Date di = new Date(0L);//L提前转化为long
+        long time = di.getTime();
+        time = time + 1000L*60*60*24*356;
+        di.setTime(time);
+        System.out.println(di);//di本身是默认时间，传入的是后移时间
+    }
+}
