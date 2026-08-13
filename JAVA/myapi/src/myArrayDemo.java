b454bf726fe80a56ebcc6ee41350abeeb1a907ac
汀烟鹭 <neha6868pal@gmail.com>
2026-08-13 11:29:09 +0800

    Add files via upload

--- JAVA/myapi/src/myArrayDemo.java
+++ JAVA/myapi/src/myArrayDemo.java
@@ -4,12 +4,16 @@ import java.util.Comparator;
 public class myArrayDemo {
     public static void main(String[] args){
         Integer[] arr = {11,21,13,4,1,3,6,23};
-        Arrays.sort(arr, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return  o2- o1;
+//        Arrays.sort(arr, new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return  o2- o1;
+//            }
+//        });
+        Arrays.sort(arr, (Integer o1, Integer o2) ->{
+                return  o1- o2;
             }
-        });
+        );
         System.out.println(Arrays.toString(arr));
     }
 }
